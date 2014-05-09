a8866c0d0306b6f5fda8ecaa9a89224013ea13bb
tknew2 <strzelewicz.alexandre@gmail.com>
2014-05-09 14:28:34 +0200

    monkey patch - i hate you solaris

--- examples/test.js
+++ examples/test.js
@@ -3,9 +3,9 @@ var pid = (process.argv[2])? parseInt(process.argv[2]): process.pid;
 
 setInterval(function() {
 
-  var options =  { keepHistory: true };
-  usage.lookup(pid, options, function(err, stat) {
+	var options =  { keepHistory: true };
+	usage.lookup(pid, options, function(err, stat) {
 
-    console.log(err, stat);
-  });
+		console.log(err, stat);
+	});
 }, 2000);
---
+++ test.js
@@ -0,0 +1,14 @@
+
+// var usage = require('.');
+
+// setInterval(function() {
+//   usage.lookup(process.pid, {keepHistory : true}, function(err, stats) {
+//     console.log(stats);
+//   });
+// }, 1000);
+
+// var os = require('os');
+// console.log(os.uptime());
+
+// var platform = os.platform();
+// console.log(platform);
